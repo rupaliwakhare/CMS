41182f7d3be8f944a39434eb6021ab3db0a9add4
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-02-02 12:35:02 +0530

    users

--- Back-End/src/config/connectDB.js
+++ Back-End/src/config/connectDB.js
@@ -1,8 +1,13 @@
+import dotenv from "dotenv";
 import mongoose from "mongoose";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL,{
+  dbName: "cms_project"  
+});
     console.log("DB Connected");
   } catch (error) {
     console.log(error);
